4e6606a33e190beadc802f319bde7142400c965c
Mikhail Khlebnikov <mikha3008@yandex.ru>
2025-04-14 14:01:42 +0300

    added solutions: 1534

--- java-solutions/src/main/java/edu/rotiez/lc/java/solution/Solution.java
+++ java-solutions/src/main/java/edu/rotiez/lc/java/solution/Solution.java
@@ -1356,4 +1356,23 @@ public class Solution {
         return node;
     }
 
+    @LeetCodeProblem(id = 1534)
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int counter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (
+                        Math.abs(arr[i] - arr[j]) <= a &&
+                        Math.abs(arr[j] - arr[k]) <= b &&
+                        Math.abs(arr[i] - arr[k]) <= c
+                    ) {
+                        counter++;
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+
 }
