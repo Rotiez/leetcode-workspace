8ff1dc9e0d00bf4b7a57b163d40d2d9f2c7e1130
Mikhail Khlebnikov <mikha3008@yandex.ru>
2025-04-16 17:24:13 +0300

    added solutions: 2537

--- java-solutions/src/main/java/edu/rotiez/lc/java/solution/Solution.java
+++ java-solutions/src/main/java/edu/rotiez/lc/java/solution/Solution.java
@@ -1448,4 +1448,20 @@ public class Solution {
         else return diff / 2;
     }
 
+    @LeetCodeProblem(id = 2537)
+    public long countGood(int[] nums, int k) {
+        Map<Integer, Integer> mpp = new HashMap<>();
+        long cnt = 0; int left = 0;
+        for (int i = 0; i < nums.length; i++) {
+            k -= mpp.getOrDefault(nums[i], 0);
+            mpp.put(nums[i], mpp.getOrDefault(nums[i], 0) + 1);
+            while (k <= 0) {
+                mpp.put(nums[left], mpp.get(nums[left]) - 1);
+                k += mpp.get(nums[left++]);
+            }
+            cnt += left;
+        }
+        return cnt;
+    }
+
 }
