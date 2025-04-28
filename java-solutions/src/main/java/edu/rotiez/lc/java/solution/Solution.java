34f12537772825a77f9c2eb19d5e0a627738f749
Mikhail Khlebnikov <mikha3008@yandex.ru>
2025-04-28 14:45:36 +0300

    added solutions: 2302

--- java-solutions/src/main/java/edu/rotiez/lc/java/solution/Solution.java
+++ java-solutions/src/main/java/edu/rotiez/lc/java/solution/Solution.java
@@ -1641,4 +1641,18 @@ public class Solution {
         return counter;
     }
 
+    @LeetCodeProblem(id = 2302)
+    public long countSubarrays(int[] nums, long k) {
+        long count = 0, sum = 0;
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum * (right - left + 1) >= k) {
+                sum -= nums[left++];
+            }
+            count += right - left + 1;
+        }
+        return count;
+    }
+
 }
